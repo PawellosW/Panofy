e992ac1b4fb7f13050737fd957160a5ba389ef6d
PawellosW <wachowskipawelek5@gmail.com>
2024-03-18 17:08:31 +0100

    utwórz projekt i gałąź VsBranch

---
+++ Panofy/Panofy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Panofy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
